b0a33301e26313f3a7e87bbcc4888c96692bf9bd
Jaroole <shakiroved@gmail.com>
2022-09-27 16:38:17 +0500

    +models

--- database/migrations/2022_09_26_094309_create_currencies_table.php
+++ database/migrations/2022_09_26_094309_create_currencies_table.php
@@ -13,14 +13,20 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->string('id')->unique();
             $table->timestamps();
+
+
+
+            $table->string('name');
+            $table->decimal('price')->unsigned();
+            $table->boolean('active')->default(true);
+            $table->timestamp('active_at')->nullable();
+            $table->integer('sort')->unsigned()->default(999);
+
+
+
         });
     }
 
@@ -31,6 +37,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('currencies');
     }
 };
